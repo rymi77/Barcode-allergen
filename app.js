641708396f8bfc9b11e9ad38adbf3cdb39995252
rymi77 <mil16035@byui.edu>
2022-01-06 16:20:36 -0700

    created a basic pages using rountes and ejs veiws

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+const path = require('path');
 const express = require('express');
+const app = express();
+const port = 5000;
+app.set('view engine', 'ejs');
+
+
+const mainRouter = require('./routes/main') 
+app.use(mainRouter);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.listen(port, function() {
+    console.log(`Example app listing on port ${port}!`)
+});
